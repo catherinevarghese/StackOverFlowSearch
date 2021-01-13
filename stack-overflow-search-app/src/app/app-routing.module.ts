c14357b74eb86a46cef2183cb7ad65091aee5d7e
catherinevarghese <catherine.v@flycatchtech.com>
2021-01-13 21:19:56 +0530

    Implemented the layout

--- stack-overflow-search-app/src/app/app-routing.module.ts
+++ stack-overflow-search-app/src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path:'listItems',loadChildren:()=>import('./list/list.module').then(m=>m.ListModule)}
-  ,{path:"**",loadChildren:()=>import('./list/list.module').then(m=>m.ListModule)}
+  {path: 'list-page', loadChildren: () => import('./list/list.module').then(m => m.ListModule)},
+  {path: 'details', loadChildren: () => import('./details/detail-page.module').then(m => m.DetailPageModule)},
+   {path: '**', loadChildren: () => import('./list/list.module').then(m => m.ListModule)}
 ];
 
 @NgModule({
