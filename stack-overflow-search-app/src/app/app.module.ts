6340e4db014e730ef920ca01665e1609c85d7b29
catherinevarghese <catherine.v@flycatchtech.com>
2021-01-13 20:29:03 +0530

    Initial commit

---
+++ stack-overflow-search-app/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NzLayoutModule,
+    CommonModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ stack-overflow-search-app/src/app/list/list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ListRoutingModule } from './list-routing.module';
+import { ListPageComponent } from './list-page/list-page.component';
+
+
+@NgModule({
+  declarations: [ListPageComponent],
+  imports: [
+    CommonModule,
+    ListRoutingModule
+  ]
+})
+export class ListModule { }
